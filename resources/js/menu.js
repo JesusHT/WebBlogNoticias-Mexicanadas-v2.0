d6776550219905c191c3e8cd2363c74d5f5b42c0
jairojpreciado <josecrack812@gmail.com>
2022-05-06 22:20:39 -0500

    rutas
    se cambiaron rutas

--- resources/js/menu.js
+++ resources/js/menu.js
@@ -8,26 +8,26 @@ html +=`      <img class="img-fluid d-block mx-auto" style="width: 30pc" src="re
         <ul class="navbar-nav me-auto mb-2 mb-md-0">
           
           <li class="nav-item">
-            <a class="nav-link" href="../php/index.php">General</a>
+            <a class="nav-link" href="includes/index.php">General</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="../php/seccionPolitica.php">Politica</a>
+            <a class="nav-link" href="includes/seccionPolitica.php">Politica</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="../php/seccionTecnologia.php">Tecnología</a>
+            <a class="nav-link" href="includes/seccionTecnologia.php">Tecnología</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="../php/seccionDeportes.php">Deporte</a>
+            <a class="nav-link" href="includes/seccionDeportes.php">Deporte</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="../php/seccionCultural.php">Cultura</a>
+            <a class="nav-link" href="includes/seccionCultural.php">Cultura</a>
           </li>
 
           <li class="nav-item dropdown">
             <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">Información</a>
             <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-              <li><a class="dropdown-item" href="../php/contacto.php">Contacto</a></li>
-              <li><a class="dropdown-item" href="../php/creadores.php">Creadores</a></li>
+              <li><a class="dropdown-item" href="includes/contacto.php">Contacto</a></li>
+              <li><a class="dropdown-item" href="includes/creadores.php">Creadores</a></li>
               <li><hr class="dropdown-divider"></li>
               <li><a class="dropdown-item" href="#">Seguridad</a></li>
             </ul>
