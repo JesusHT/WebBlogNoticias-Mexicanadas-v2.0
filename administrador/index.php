2b0bba1b43f4566bae47a8fb624287ef4bdd04f0
jairojpreciado <josecrack812@gmail.com>
2022-05-06 21:45:55 -0500

    qwerty
    qwerty

--- administrador/index.php
+++ administrador/index.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://unpkg.com/vanilla-datatables@latest/dist/vanilla-dataTables.min.css" rel="stylesheet" type="text/css">
     <script src="https://unpkg.com/vanilla-datatables@latest/dist/vanilla-dataTables.min.js" type="text/javascript"></script>
-    <title>Mexicanadas</title>
+    <title>Mexicanadas   </title>
 </head>
 <body>
     
